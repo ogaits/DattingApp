7d73cd76520872e1198ed01ba31b5c53f570672f
ogaits <tiagosantos60@gmail.com>
2019-10-02 10:28:46 +0100

    Commit End of Section 6

--- DattingApp-SPA/src/app/nav/nav.component.ts
+++ DattingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
-import { AuthService } from './../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { AlertifyService } from '../_services/alertify.service';
+import { AuthService } from './../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
