15ae81578ef505b19fa5528c5296f6c9c3696ea5
ogaits <tiagosantos60@gmail.com>
2019-10-07 15:23:32 +0100

    Commit end of section 10

--- DattingApp.API/Controllers/UsersController.cs
+++ DattingApp.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DattingApp.API.Data;
@@ -37,6 +39,22 @@ namespace DattingApp.API.Controllers
             var userToReturn = mapper.Map<UserForDetailedDto>(user);
 
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+            var userFromRepo = await repo.GetUser(id);
+
+            mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await repo.SaveAll())
+                return NoContent();
+            
+            throw new Exception($"Updating user {id} failed on save");
         }
     }
 }
