1856e808a5d587565df7c688ed4bdae3ccc234ce
ogaits <tiagosantos60@gmail.com>
2019-10-09 17:06:47 +0100

    Section 11 EP 9

--- DattingApp.API/Helpers/AutoMapperProfiles.cs
+++ DattingApp.API/Helpers/AutoMapperProfiles.cs
@@ -25,6 +25,8 @@ namespace DattingApp.API.Helpers
                 });
             CreateMap<Photo, PhotosForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();
         }
     }
 }
